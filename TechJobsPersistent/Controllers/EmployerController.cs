f696b6b34fd1df7fabc0800cfbbcd81b5175bc7c
Ella Vankirk <ella.vankirk@gmail.com>
2021-02-03 20:24:17 -0600

    Made most of the controller changes. Need to fix the About method in the Employer Controller

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,24 +13,45 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employer = context.Employers.ToList();
+            return View(employer);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployer = new AddEmployerViewModel();
+            return View(addEmployer);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployer)
         {
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployer.Name,
+                    Location = addEmployer.Location
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+            }
+            //TODO: Add a redirect ?
             return View();
         }
 
         public IActionResult About(int id)
         {
+
             return View();
         }
     }
